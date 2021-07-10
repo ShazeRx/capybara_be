10febae6e30c9489591b601c643dc95de5fbaf54
ShazeRx <perkowskikrzysio@gmail.com>
2021-07-10 10:22:16 +0200

    HEROKU url fix

--- cpbra_be/settings.py
+++ cpbra_be/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'secret')
 # DEBUG CONFIG
 DEBUG = os.environ.get('DEBUG') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [os.environ.get('BASE_URL', '127.0.0.1')]
 
 # Application definition
 
