8c9c49316f8d7f90102224343efe9608dda17020
ShazeRx <perkowskikrzysio@gmail.com>
2021-08-04 13:03:48 +0200

    CPBRA-36, added channels authentication

--- cpbra_be/asgi.py
+++ cpbra_be/asgi.py
@@ -1,13 +1,13 @@
 import os
 
-from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 
 import cpbra.chat.routing
 from cpbra import chat
+from cpbra_be.channelsmiddleware import TokenAuthMiddleware
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cpbra_be.settings')
 
 application = ProtocolTypeRouter({"http": get_asgi_application(),
-                                  "websocket": AuthMiddlewareStack(URLRouter(chat.routing.websocket_urlpatterns))})
+                                  "websocket": TokenAuthMiddleware(URLRouter(chat.routing.websocket_urlpatterns))})
