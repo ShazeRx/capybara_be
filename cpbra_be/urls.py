54315a108c10a30775d586e3878c69037e7525e2
ShazeRx <perkowskikrzysio@gmail.com>
2021-08-10 11:04:52 +0200

    static urls fix

--- cpbra_be/urls.py
+++ cpbra_be/urls.py
@@ -1,10 +1,13 @@
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+
+from cpbra_be import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('schema/', SpectacularAPIView.as_view(), name='schema'),
     path('swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('', include('cpbra.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
